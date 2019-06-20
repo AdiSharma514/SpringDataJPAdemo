ed036b0a5f828cfaa513a9010796ac34680cd3a1
Aditya Sharma <sharma.aditya514@gmail.com>
2019-06-20 10:58:01 +0530

    Downloaded skeleton from Spring Initializr

---
+++ src/main/java/com/gfg/springJPAdemo/SpringDataJPAdemo/SpringDataJpAdemoApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.springJPAdemo.SpringDataJPAdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpAdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpAdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfg/springJPAdemo/SpringDataJPAdemo/SpringDataJpAdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gfg.springJPAdemo.SpringDataJPAdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataJpAdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
